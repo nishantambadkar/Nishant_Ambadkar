db7737bac5f8ba0987e68a7ddaf910556ede1aca
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:03:12 +0530

    sum of two no

---
+++ 31 march/sum of two no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int num1, num2, res;
+	printf("enteer num1");
+	scanf("%d", &num1);
+	printf("enter num2");
+	scanf("%d", &num2);
+	res =num1+num2;
+	printf("sum is %d", res);
+	getch();
+}
+
