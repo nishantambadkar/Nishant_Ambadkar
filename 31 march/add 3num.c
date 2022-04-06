bf4cef80fa007582b07c26a15a4a01831bcafae4
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:33:43 +0530

    3 NO. ADDITION

---
+++ 31 march/add 3num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int num1, num2, num3, res;
+	printf("enter num1: ");
+	scanf("%d", &num1);
+	printf("enter num2: ");
+	scanf("%d", &num2);
+	printf("enter num3: ");
+	scanf("%d", &num3);
+	res=num1+num2+num3;
+	printf("addition of 3 num %d", res);
+	getch();
+}
