af9c41d9f91d4b70a5a9650632e9096701211b1e
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 13:58:50 +0530

    voltage

---
+++ 31 march/voltage.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r, i, v;
+	printf("enter value of r");
+	scanf("%d", &r);
+	printf("enter value of i");
+	scanf("%d", &i);
+	v= r*i+;
+	printf("voltage = %d v");
+	getch();
+}
