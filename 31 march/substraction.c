4682cede95ff335e8941f07b2d76e4bf1b52f148
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:24:56 +0530

    SUBSTRACT NUMBERS

---
+++ 31 march/substraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int num1, num2, result;
+	printf("enter num1: ");
+	scanf("%d", &num1);
+	printf("enter num2:");
+	scanf("%d", &num2);
+	result=num1-num2;
+	printf("result= %d",result);
+	getch();
+}
