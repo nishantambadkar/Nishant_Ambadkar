e57a2aae9a58bdffe8ae510b53370d0f9c6a7d14
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:31:56 +0530

    AREA OF EQ TRANGLE

---
+++ 31 march/area of eq trangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int side, area;
+	printf("enter the side in cm");
+	scanf("%d",&side);
+	area=side*side*side*side;
+	printf("area of eq trangle %d cubic cm");
+}
