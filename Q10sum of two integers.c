5b0c7328a1f539b64c737617e0605566f9625211
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:21:00 +0530

    A1Q10

---
+++ Q10sum of two integers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int int1, int2, res;
+	printf("enteer int1");
+	scanf("%d", &int1);
+	printf("enter int2");
+	scanf("%d", &int2);
+	res =int1+int2;
+	printf("sum is %d", res);
+	getch();
+}
