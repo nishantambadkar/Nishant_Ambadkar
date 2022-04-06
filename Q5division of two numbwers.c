bd851efde858de66a9e776135f7bef483150a562
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-06 14:18:53 +0530

    A1Q5

---
+++ Q5division of two numbwers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	float num1, num2, res;
+	printf("enter num1_");
+	scanf("%f", &num1);
+	printf("enter num2_");
+	scanf("%f", &num2);
+	res =num1/num2;
+	printf("sum is %f", res);
+	getch();
+}
