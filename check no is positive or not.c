f9882432bebad1d4eeaf3d69cb21c731a0f4ba8c
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-12 09:41:45 +0530

    add even odd

---
+++ check no is positive or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("enter number");
+	scanf("%d",&num);
+	if(num<0)
+	{
+	printf("number is negative %d", num);
+    }
+    else
+    {
+    	printf("no is positive",num);
+	}
+	
+}
