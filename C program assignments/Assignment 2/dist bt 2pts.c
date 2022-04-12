c09562aee3b176e9fb025dbadaf3d2e33db2c544
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-12 11:49:16 +0530

    A2Q15

---
+++ C program assignments/Assignment 2/dist bt 2pts.c
@@ -0,0 +1,12 @@
+//wap to calculate dist betw 2 points
+#include<stdio.h>
+void main()
+{
+	int num1 ,num2, d;
+	printf("enter 1st point");
+	scanf("%d",&num1);
+	printf("enter 2nd point");
+	scanf("%d",&num2);
+	d=num1-num2;
+	printf("dist betw points is %d",d);
+}
