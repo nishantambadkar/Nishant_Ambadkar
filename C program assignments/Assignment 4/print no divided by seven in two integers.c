4720db145fcb9a9dda5a481962c1a731610ad7dc
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-18 11:34:29 +0530

    A4Q36

---
+++ C program assignments/Assignment 4/print no divided by seven in two integers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int num1, num2, i;
+	printf("enter 1st num");
+	scanf("%d",&num1);
+	printf("enter 2nd num");
+	scanf("%d",&num2);
+	i=num1;
+	while(i<=num2)
+	{
+		if(i%7==2 || i%7
+		==3)
+		{
+			printf("%d\n",i);
+		}
+		i++;
+	}
+}
