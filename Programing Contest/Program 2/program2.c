13e2f67f562c2df0d9cd2ea6864b26aa09d37751
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-24 17:48:15 +0530

    Program 2
    Program to Count the occurance of digit in entered string

---
+++ Programing Contest/Program 2/program2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+void main ()
+{
+    char input_str[10], ch;
+    int num, len, output_str[10]= {0,0,0,0,0,0,0,0,0,0};
+    system("cls");
+    
+    printf("Enter the string: ");
+    gets(input_str);
+    
+    len=strlen(input_str);
+    for(int i=0; i<len; i++)
+    {
+        ch=input_str[i];
+        
+        if(ch>=48 && ch<=57)
+        {
+            num=(char)ch-48;
+           
+            if(output_str[num])
+            {
+            output_str[num]=output_str[num]+1;
+            }
+            else
+            {
+            output_str[num]=1;
+            }
+        }
+    }
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ",output_str[i]);
+    }   
+ 
+}
