502da4937f031f95228ca84fbd44e7a15ed1940a
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-25 10:59:29 +0530

    P8

---
+++ C program assignments/various patterns by for loop/pattern 8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=5;c++)
+		{
+			if(r==2 || r==4)
+			printf("a");
+			else
+			printf("%d",c);
+		}
+		printf("\n");
+	}
+}
