642f84b40744f5b62ff79c8a3561df1d17e11144
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-25 11:02:35 +0530

    P10

---
+++ C program assignments/various patterns by for loop/print pyramid by star.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int r,sp,str;
+	for(r=1;r<=4;r++)
+	{
+		for(sp=1;sp<=41-r;sp++)
+		{
+			printf(" ");
+		}
+		for(str=1;str<=r;str++)
+		{
+			printf(" *");
+		}
+		printf("\n");
+	}
+}
