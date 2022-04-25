03471809665c3378287001d0122125465288a233
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-25 10:48:17 +0530

    search pr CW

---
+++ search prog.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char sym;
+	printf("enter symbol");
+	scanf("%c",&sym);
+	
+	if("()" && "[]" && "{}")
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
