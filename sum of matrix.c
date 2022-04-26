978aa3f956c983d5d2c697dc1e60544df2c8d096
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-04-26 14:50:58 +0530

    CW SUM OF MATRIX

---
+++ sum of matrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+	int m1[2][2],m2[2][2],res[2][2],i,j;
+	printf("enter 2*2 matrix m1");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m1[%d][%d]=",i,j);
+			scanf("%d",&m1[i][j]);
+		}
+	}
+	printf("enter 2*2 matrix m2");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m2[%d][%d]=",i,j);
+			scanf("%d",&m2[i][j]);
+		}
+	}
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			res[i][j]=m1[i][j] + m2[i][j];
+		}
+	}
+	//display
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+		    	printf("res[%d][%d]=%d",i,j,res[i][j]);
+		}
+	}
+}
