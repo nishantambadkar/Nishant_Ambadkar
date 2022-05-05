0051c9f0f95df9cda7c00d49a8c16d25479ee951
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-05-05 11:25:40 +0530

    fun sample

---
+++ function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void disname(); //giving info to compiler about your function
+void main()
+{
+	disname();
+	disname();
+	disname();
+}
+void disname()
+{
+	printf("Nishant");
+	getch();
+}
