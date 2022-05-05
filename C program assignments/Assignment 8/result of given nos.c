64785bab9394733bce2328e62ecbb3b2056d3c30
nishantambadkar <102666042+nishantambadkar@users.noreply.github.com>
2022-05-05 11:28:55 +0530

    Q03

---
+++ C program assignments/Assignment 8/result of given nos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	float r1, r2, r3, r4;
+	
+	r1=-1+4*6;
+	r2=(35+5)%7;
+	r3=14+(-4)*6/11;
+	r4=2+15/6*1-7%2;
+	
+	printf("result is \n%f \n%f \n%f \n%f",r1,r2,r3,r4);
+	
+}
